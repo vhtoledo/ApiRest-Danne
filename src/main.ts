8c057ce3243b3ffdd85c2dca704228d8ed27e15c
vhtoledo <querynetworking@gmail.com>
2023-09-07 12:23:56 -0300

    creacion de product DTO

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Prefijo
+  app.setGlobalPrefix('api');
+
+  // Configuracion global de pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+   );
+
   await app.listen(3000);
 }
 bootstrap();
