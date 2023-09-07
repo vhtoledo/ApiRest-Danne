d42b483726a5b8462784181f49fde0f02c0c4e3d
vhtoledo <querynetworking@gmail.com>
2023-09-07 12:39:50 -0300

    insertar usando typeORM

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  
+  constructor(
+
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+
+    try {
+
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save( product );
+
+      return product;
+      
+    } catch (error) {
+      console.log(error)
+      throw new InternalServerErrorException('ayuda');
+    }
   }
 
   findAll() {
@@ -23,4 +45,5 @@ export class ProductsService {
   remove(id: number) {
     return `This action removes a #${id} product`;
   }
+  
 }
