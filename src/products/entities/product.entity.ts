9ef6f1d42481e9d06eb48b69759d1c2b7926a083
vhtoledo <querynetworking@gmail.com>
2023-09-07 11:28:43 -0300

    TypeORM - Entity - Product

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,13 @@
+import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('text', {
+        unique: true,
+    })
+    title: string;
+}
