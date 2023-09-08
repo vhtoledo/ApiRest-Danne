43ab944d21713230af558699209b2976080cf723
vhtoledo <querynetworking@gmail.com>
2023-09-08 09:12:16 -0300

    paginacion de productos TypeOrm

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
 
 @Injectable()
 export class ProductsService {
@@ -33,8 +34,14 @@ export class ProductsService {
   }
 
   // Obtener todos los productos
-  findAll() {
-    return this.productRepository.find({});
+  findAll( paginationDto:PaginationDto ) {
+    const { limit = 10, offset = 0 } = paginationDto;
+
+    return this.productRepository.find({
+      take: limit,
+      skip: offset,
+      // TODO: relaciones
+    })
   }
 
   // Obtener producto por id
