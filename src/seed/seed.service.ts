594a3f649ecd07bf1374f2e70685d53671316018
vhtoledo <querynetworking@gmail.com>
2023-09-12 10:18:55 -0300

    implementacion de product seed

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { ProductsService } from 'src/products/products.service';
+
+@Injectable()
+export class SeedService {
+
+  constructor(
+    private readonly productsService: ProductsService
+  ) {}
+  
+  async runSeed() {
+
+    await this.insertNewProducts();
+
+    return 'SEED EXECUTED';
+  }
+
+  private async insertNewProducts() {
+    await this.productsService.deleteAllProducts();
+
+    return true;
+  }
+}
