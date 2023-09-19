73357dd7899a0bd8b72fe12c22162d0e392464dd
vhtoledo <querynetworking@gmail.com>
2023-09-19 09:01:29 -0300

    validar archivos a subir

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { FilesService } from './files.service';
+import { fileFilter } from './helpers/fileFilter.helper';
 
 
 @Controller('files')
@@ -8,10 +9,14 @@ export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('product')
-  @UseInterceptors( FileInterceptor('file'))
+  @UseInterceptors( FileInterceptor('file', {
+    fileFilter: fileFilter
+  }) )
   uploadProductImage(
     @UploadedFile() file: Express.Multer.File
   ) {
-    return file;
+    return {
+      fileName: file.originalname
+    }; 
   }
 }
