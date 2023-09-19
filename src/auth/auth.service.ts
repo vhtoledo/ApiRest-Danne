a2bcc9fcbacd2ca0d375d3ddb5fe5b2bd47eb2ef
vhtoledo <querynetworking@gmail.com>
2023-09-19 12:40:01 -0300

    encriptar la contraseña usando bcrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,8 @@ import { BadRequestException, Injectable, InternalServerErrorException } from '@
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
+import * as bcrypt from 'bcrypt';
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 
@@ -20,9 +22,15 @@ export class AuthService {
     
     try {
 
-      const user = this.userRepository.create( createUserDto );
+      const { password, ...userData } = createUserDto;
+
+      const user = this.userRepository.create({
+        ...userData, 
+        password: bcrypt.hashSync( password, 10)
+      });
 
       await this.userRepository.save( user );
+      delete user.password;
 
       return user;
       
