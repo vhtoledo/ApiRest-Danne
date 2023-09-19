986890862cff7a32dc4782bd3a2e672d64870828
vhtoledo <querynetworking@gmail.com>
2023-09-19 08:49:23 -0300

    subir archivo al backend

---
+++ src/files/files.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { FilesService } from './files.service';
+
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('product')
+  @UseInterceptors( FileInterceptor('file'))
+  uploadProductImage(
+    @UploadedFile() file: Express.Multer.File
+  ) {
+    return file;
+  }
+}
