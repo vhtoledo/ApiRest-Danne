3f396db00b548ad99f94b860bae6f8f259354a27
vhtoledo <querynetworking@gmail.com>
2023-09-20 12:18:05 -0300

    Nest Authentication - Passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
@@ -9,7 +11,16 @@ import { User } from './entities/user.entity';
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
-    TypeOrmModule.forFeature([ User ])
+    TypeOrmModule.forFeature([ User ]),
+
+    PassportModule.register({ defaultStrategy: 'jwt'}),
+
+    //JwtModule.register({
+    //  secret: process.env.JWT_SECRET,
+    //  signOptions: {
+    //    expiresIn:'2h'
+    //  }
+    //})
   ],
   exports: [ TypeOrmModule ]
 })
